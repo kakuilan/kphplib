83b9cc7b5db0262f7f9999be259beb32a884c658
kakuilan <kakuilan@163.com>
2017-08-07 16:16:32 +0800

    dev

--- src/LkkRedisQueueService.php
+++ src/LkkRedisQueueService.php
@@ -22,9 +22,9 @@ class LkkRedisQueueService extends LkkService {
     const REDIS_QUEUE_TRANS_LOCKKEY = 'trans_lock'; //中转队列的锁key
     const REDIS_QUEUE_TRANS_LOCKTIM = 3600; //中转队列的锁时间,秒
 
-    private $redis; //redis客户端对象
-    private $redisConf; //redis配置
-    private $timeout = 2.5;
+    protected $redis; //redis客户端对象
+    protected $redisConf; //redis配置
+    protected $timeout = 2.5;
     private static $prefix = 'que_';
     private static $allQuekeys = []; //当前类用过的所有队列key
     private $curQueName = ''; //当前队列key
