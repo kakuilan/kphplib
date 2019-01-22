f18304de6dc3aba9a7797732a73c317b1fd03401
kakuilan <kakuilan@163.com>
2019-01-22 11:39:19 +0800

    add StringHelper::StrToBin/BinToStr

--- src/Helpers/StringHelper.php
+++ src/Helpers/StringHelper.php
@@ -595,6 +595,38 @@ class StringHelper {
     }
 
 
+    /**
+     * 将字符串转换成二进制
+     * @param $str
+     * @return string
+     */
+    public static function StrToBin($str){
+        //列出每个字符
+        $arr = preg_split('/(?<!^)(?!$)/u', $str);
+        //unpack字符
+        foreach($arr as &$v){
+            $temp = unpack('H*', $v);
+            $v = base_convert($temp[1], 16, 2);
+            unset($temp);
+        }
+
+        return join(' ', $arr);
+    }
+
+
+    /**
+     * 将二进制转换成字符串
+     * @param $str
+     * @return string
+     */
+    public static function BinToStr($str){
+        $arr = explode(' ', $str);
+        foreach($arr as &$v){
+            $v = pack("H".strlen(base_convert($v, 2, 16)), base_convert($v, 2, 16));
+        }
+
+        return join('', $arr);
+    }
 
 
 
